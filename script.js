860e63bd2f5d5867c64e461b4f518dadf05660cb
HuseynAlizade <huseynalizade2010@gmail.com>
2024-08-13 12:36:43 +0400

    jstask4

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//const ex1 = +prompt("Please enter you name");
+//const ex2 = prompt("Please enter your name");
+
+//if( ex1 == ex2){
+ //   console.log("same");
+//}else{
+  //  console.log("difrent");
+//}
+
+const array = [0,1,2,3,4,5,6,7,8,9,10]
+let count = 0
+
+for( let i = 0 ;i<array.length ;i++){
+    count += array[i];
+}
+console.log(count);
